6701448c72a2e24bacc4606ef5106a6b0d5194b9
EvaMsanchez <eva_crm20@hotmail.com>
2024-01-12 12:01:21 +0100

    first commit

---
+++ src/app/components/generaciones/generaciones.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-generaciones', 
+    template: '<div><p>Este es el componente generaciones</p></div>' 
+})
+
+export class GeneracionesComponent{}
+
---
+++ src/app/components/pokemons/pokemons.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-pokemons', 
+    template: '<div><p>Este es el componente pokemons</p></div>' 
+})
+
+export class PokemonsComponent{}
+
