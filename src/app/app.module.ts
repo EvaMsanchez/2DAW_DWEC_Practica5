ced0a7756a12ea086f01d9568dadbd096491a55d
EvaMsanchez <eva_crm20@hotmail.com>
2024-02-18 20:14:06 +0100

    práctica 10 - formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PokemonsComponent } from './components/pokemons/pokemons.component';
 
 import { registerLocaleData } from '@angular/common';
 import es from '@angular/common/locales/es';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
 
 registerLocaleData(es);
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonsComponent
+    PokemonsComponent,
+    PokemonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [{provide: LOCALE_ID, useValue: 'es'}],
   bootstrap: [AppComponent]
--- src/app/components/pokemons/pokemons.component.ts
+++ src/app/components/pokemons/pokemons.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonsService } from 'src/app/services/pokemons.service';
+import { FormBuscarPersonaje } from 'src/app/models/form-buscar-personaje';
 
 @Component({
   selector: 'app-pokemons',
@@ -8,24 +10,32 @@ import { Component, OnInit } from '@angular/core';
 
 export class PokemonsComponent implements OnInit{
 
-  pokemons = [
-    { 'id': 1, 'nombre': 'Bulbasaur', 'categoria': 'grano', 'tipo': 'planta, venenosa',
-    'talla': '', 'peso': 6.9, 'imagen': './assets/01.png' },
-    { 'id': 2, 'nombre': 'Ivysaur', 'categoria': 'grano', 'tipo': 'planta, venenosa',
-    'talla': '', 'peso': 13, 'imagen': './assets/02.png' },
-    { 'id': 3, 'nombre': 'Venusaur', 'categoria': 'grano', 'tipo': 'planta, venenosa',
-    'talla': 2, 'peso': '', 'imagen': './assets/03.png' },
-    { 'id': 4, 'nombre': 'Charmander', 'categoria': 'lagarto', 'tipo': 'fuego',
-    'talla': 0.6, 'peso': 8.5, 'imagen': './assets/04.png' },
-    { 'id': 5, 'nombre': 'Charmaleon', 'categoria': 'llama', 'tipo': 'fuego',
-    'talla': 1.1, 'peso': '', 'imagen': './assets/05.png' },
-    { 'id': 6, 'nombre': 'Charizard', 'categoria': 'llama', 'tipo': 'fuego, vuelo',
-    'talla': 1.7, 'peso': 90.5, 'imagen': './assets/06.png' }
-  ];
+  pokemonSeleccionado = '';
 
-  constructor() {}
+  pokemons = [{'id': 0, 'nombre': '', 'imagen': '', 'categoria': '', 'tipo': '', 
+               'talla': 0, 'peso': 0}];
+ 
+  cuenta = 0;
 
-  ngOnInit() {}
+  formBuscarPersonaje = new FormBuscarPersonaje("");
+
+  constructor(private servicePokemons: PokemonsService) {}
+
+  ngOnInit() {
+    this.pokemons = this.servicePokemons.getPokemons();
+    this.cuenta = this.servicePokemons.contarPokemons();
+  }
+
+  buscar(nombreDelPersonaje: string) 
+  {
+    this.pokemons = this.servicePokemons.buscarPersonaje(nombreDelPersonaje);
+  }
+
+  cancelarBuscar() 
+  {
+    this.pokemons = this.servicePokemons.getPokemons();
+    this.formBuscarPersonaje.setNombre('');
+  }
 }
 
 
